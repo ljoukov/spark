eb87e81e159059a1383e502111eb2e3c8b13c842
Liudmila Joukovskaya <liudmila.joukovskaya@gmail.com>
2025-09-21 18:57:51 -0700

    delete /auth/start

--- web/src/routes/auth/logout/+server.ts
+++
@@ -1,8 +0,0 @@
-import type { RequestHandler } from './$types';
-import { clearUserAuthCookie } from '$lib/server/auth/cookie';
-import { clientSideRedirect } from '$lib/server/utils/response';
-
-export const GET: RequestHandler = async ({ url, cookies }) => {
-	clearUserAuthCookie(cookies);
-	return clientSideRedirect(new URL('/', url));
-};
--- web/src/routes/auth/relogin/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { clearUserAuthCookie } from '$lib/server/auth/cookie';
-import type { PageServerLoad } from './$types';
-
-export const load = (async ({ cookies, url }) => {
-	clearUserAuthCookie(cookies);
-	const r = url.searchParams.get('r') || '/app';
-	return { r };
-}) satisfies PageServerLoad;
