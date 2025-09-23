d5b882e4bac2ad63c0bc4c7e4f24d9983950a85e
Liudmila Joukovskaya <liudmila.joukovskaya@gmail.com>
2025-09-23 02:42:22 -0700

    [fix/admin] modernize sample quiz previews

---
+++ web/src/lib/components/ui/popover/index.ts
@@ -0,0 +1,8 @@
+import { Popover as PopoverPrimitive } from 'bits-ui';
+import Content from './popover-content.svelte';
+import Trigger from './popover-trigger.svelte';
+
+const Root = PopoverPrimitive.Root;
+const Anchor = PopoverPrimitive.Anchor;
+
+export { Anchor, Content, Root, Trigger };
