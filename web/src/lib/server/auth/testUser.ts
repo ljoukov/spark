312819523d1ebb8eb4caccbe7040b463ff5384d7
Liudmila Joukovskaya <liudmila.joukovskaya@gmail.com>
2025-10-05 17:33:50 -0700

    FORCE_TEST_USER

--- web/src/lib/server/auth/testUser.ts
+++ web/src/lib/server/auth/testUser.ts
@@ -6,20 +6,32 @@ const testUserIdRegex = /^(test-(admin|free|paid)-[A-Za-z0-9]{16})$/;
 
 const testUserSchema = z.union([z.undefined(), z.string().regex(testUserIdRegex)]);
 
-const testUser = testUserSchema.parse(env['TEST_USER']);
+const testUserId = (() => {
+	if (
+		env['FORCE_TEST_USER'] === undefined ||
+		env['FORCE_TEST_USER'].toLocaleLowerCase() === 'false'
+	) {
+		return undefined;
+	}
+	const id = testUserSchema.parse(env['TEST_USER']);
+	if (id !== undefined) {
+		console.log('testUserId: running as test user');
+	}
+	return id;
+})();
 
 export function isTestUserAdmin(): boolean {
-	return testUser !== undefined && testUser.startsWith('test-admin-');
+	return testUserId !== undefined && testUserId.startsWith('test-admin-');
 }
 
 export function isTestUser(): boolean {
-	return testUser !== undefined;
+	return testUserId !== undefined;
 }
 
 export function getTestUserId(): string {
-	if (testUser === undefined) {
+	if (testUserId === undefined) {
 		console.error('No test user ID set');
 		throw new Error('No test user ID set');
 	}
-	return testUser;
+	return testUserId;
 }
