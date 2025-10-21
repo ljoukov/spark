cabb309dfad329de8253e1f0fe783b20a0855aea
Liudmila Joukovskaya <liudmila.joukovskaya@gmail.com>
2025-10-20 22:23:14 -0700

    write_dotenv.py

--- web/write_dotenv.py
+++ web/write_dotenv.py
@@ -2,7 +2,7 @@ import os
 
 dotenv = os.environ["DOTENV"]
 print(f"DOTENV is None: {dotenv is None}")
-file_name = "web/.env.local"
-print(f"Writing {file_name}: len={len(dotenv)}")
-with open(file_name, "w") as f:
-    f.write(dotenv)
+# file_name = ".env.local"
+# print(f"Writing {file_name}: len={len(dotenv)}")
+# with open(file_name, "w") as f:
+#     f.write(dotenv)
