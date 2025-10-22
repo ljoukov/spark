b878218a613a312fe592c1c9f3f39359585ad9dc
Liudmila Joukovskaya <liudmila.joukovskaya@gmail.com>
2025-10-21 21:59:15 -0700

    [refactor/llm] move story pipeline into package

--- eval/src/quiz/legacy/prepare-input.ts
+++ eval/src/quiz/legacy/prepare-input.ts
@@ -21,10 +21,10 @@ import { GeminiModelId } from "@spark/llm/utils/gemini";
 import {
   runJobsWithConcurrency,
   type JobProgressReporter,
-} from "../../utils/concurrency";
+} from "@spark/llm/utils/concurrency";
 import { WORKSPACE_PATHS, ensureEvalEnvLoaded } from "../../utils/paths";
 import { detectMimeType } from "../../utils/mime";
-import { convertGooglePartsToLlmParts, generateJson } from "../../utils/llm";
+import { convertGooglePartsToLlmParts, generateJson } from "@spark/llm/utils/llm";
 import { createCliCommand, createIntegerParser } from "../../utils/cli";
 
 ensureEvalEnvLoaded();
