1aa01e47212eac05dc9c9cf8032bd8307f7456a4
Liudmila Joukovskaya <liudmila.joukovskaya@gmail.com>
2026-01-30 12:34:32 +0000

    [feat/ios] add firebase bootstrap and auth screen

--- CheckMate/CheckMate/CheckMateApp.swift
+++ CheckMate/CheckMate/CheckMateApp.swift
@@ -1,10 +1,25 @@
+import FirebaseCore
 import SwiftUI
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct CheckMateApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @StateObject private var firebaseClients = FirebaseClients()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(firebaseClients)
         }
     }
 }
