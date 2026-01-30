bec21a1ae6da89dd24acf35cc8cbb7a8a1322afa
Liudmila Joukovskaya <liudmila.joukovskaya@gmail.com>
2026-01-30 12:47:40 +0000

    [feat/ios] implement Apple and Google sign-in

--- CheckMate/CheckMate/CheckMateApp.swift
+++ CheckMate/CheckMate/CheckMateApp.swift
@@ -1,4 +1,5 @@
 import FirebaseCore
+import GoogleSignIn
 import SwiftUI
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
@@ -7,7 +8,21 @@ final class AppDelegate: NSObject, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
         FirebaseApp.configure()
+        if let clientID = FirebaseApp.app()?.options.clientID {
+            GIDSignIn.sharedInstance.configuration = GIDConfiguration(clientID: clientID)
+        }
         return true
+    }
+
+    func application(
+        _ application: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+    ) -> Bool {
+        if GIDSignIn.sharedInstance.handle(url) {
+            return true
+        }
+        return false
     }
 }
 
